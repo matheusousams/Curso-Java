bec5fe08a0b19b620c40b2965f177398196b4cb8
Matheus <matheusousams.98@gmail.com>
2022-07-24 19:42:16 -0300

    Commit 24/07/2022

---
+++ Curso Java/exercicios2/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class PrimeiroTrauma {
+    
+    int a = 3;
+    static int b = 4;
+    public static void main(String[] args) {
+        
+        PrimeiroTrauma pt = new PrimeiroTrauma();
+        System.out.println(pt.a);
+
+        System.out.println(b);
+    }
+}
