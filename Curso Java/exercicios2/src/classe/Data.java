1405c72fe3a56621949a701e930122c254ff2026
Matheus <matheusousams.98@gmail.com>
2022-08-07 19:15:24 -0300

    Aula 111

--- Curso Java/exercicios2/src/classe/Data.java
+++ Curso Java/exercicios2/src/classe/Data.java
@@ -7,18 +7,23 @@ public class Data {
     int ano;
 
     Data(){
-        dia = 1;
-        mes = 1;
-        ano = 1970;
+        //dia = 1;
+        //mes = 1;
+        //ano = 1970;
+        this(1, 1, 1970);
     }
 
     Data(int d, int m, int a){
-        dia = d;
-        mes = m;
-        ano = a;
+        this.dia = d;
+        this.mes = m;
+        this.ano = a;
     }
 
     String obterDataFormatada() {
         return String.format("%d/%d/%d", dia, mes, ano);
+    }
+
+    void imprimirDataFormatada() {
+        System.out.println(obterDataFormatada());
     }
 }
