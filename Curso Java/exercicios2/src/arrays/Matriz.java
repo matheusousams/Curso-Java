95696cdcd30a64f348485c8fe950d2bfdf62f947
Matheus <matheusousams.98@gmail.com>
2022-08-27 11:56:52 -0300

    Aula 125 - Matriz

---
+++ Curso Java/exercicios2/src/arrays/Matriz.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Matriz {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos alunos? ");
+        int qtdAlunos = scanner.nextInt();
+
+        System.out.println("Quantas notas por aluno?");
+        int qtdNotas = scanner.nextInt();
+
+        double[][] notasDaTurma = new double[qtdAlunos][qtdNotas];
+
+        double total = 0;
+        for(int a = 0; a < notasDaTurma.length; a++) {
+            for (int n = 0; n < notasDaTurma[a].length; n++) {
+                System.out.printf("Informe a nota %d do aluno %d: ", n+1, a+1);
+                notasDaTurma[a][n] = scanner.nextDouble();
+                total += notasDaTurma[a][n];
+            }
+        }
+        double media = total / (qtdAlunos * qtdNotas);
+        System.out.println("Media da turma é: " + media);
+
+        scanner.close();
+    }
+}
